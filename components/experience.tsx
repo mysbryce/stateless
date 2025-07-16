4f8d28f3f645a28029d3111eb2d34d8b4840c366
999s <123440999+mysbryce@users.noreply.github.com>
2025-07-16 21:18:54 +0700

    chore: update exp

--- components/experience.tsx
+++ components/experience.tsx
@@ -97,7 +97,7 @@ export default function Experience() {
                     <hr />
                     <div className="flex flex-col timeline-start timeline-box">
                         <span>Freelancer & MSC Shop (FiveM)</span>
-                        <span className="text-zinc-400">Now - WFH Fulltime</span>
+                        <span className="text-zinc-400">2025 - WFH Fulltime</span>
                     </div>
                     <div className="timeline-middle">
                         <svg
@@ -113,6 +113,28 @@ export default function Experience() {
                             />
                         </svg>
                     </div>
+                </li>
+                <li>
+                    <hr />
+                    <div className="timeline-middle">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="h-5 w-5"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                clipRule="evenodd"
+                            />
+                        </svg>
+                    </div>
+                    <div className="flex flex-col timeline-end timeline-box">
+                        <span>Freelancer & Grand City</span>
+                        <span className="text-zinc-400">Now - WFH Fulltime</span>
+                    </div>
+                    <hr />
                 </li>
             </ul>
         </div>
